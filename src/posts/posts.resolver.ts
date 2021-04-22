62ab209a02fd2077384cc8ea6a18fd331ca20ab8
minkj1992 <minkj1992@gmail.com>
2021-04-22 21:50:01 +0900

    feat: first dummy createPost Mutation with subscription

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,16 +1,39 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  Subscription,
+} from '@nestjs/graphql';
 import { PostsService } from './posts.service';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { Inject } from '@nestjs/common';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { PUB_SUB } from 'src/pub-sub/pub-sub.module';
+
+const POST_ADDED_EVENT = 'postAdded';
 
 @Resolver(() => Post)
 export class PostsResolver {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(
+    private readonly postsService: PostsService,
+    @Inject(PUB_SUB) private pubSub: RedisPubSub,
+  ) {}
+
+  @Subscription(() => Post)
+  postAdded() {
+    return this.pubSub.asyncIterator(POST_ADDED_EVENT);
+  }
 
   @Mutation(() => Post)
-  createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
-    return this.postsService.create(createPostInput);
+  async createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
+    console.log(createPostInput);
+    const newPost = await this.postsService.create(createPostInput);
+    this.pubSub.publish(POST_ADDED_EVENT, { postAdded: newPost });
+    return newPost;
   }
 
   @Query(() => [Post], { name: 'posts' })
