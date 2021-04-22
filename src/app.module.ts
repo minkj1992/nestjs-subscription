8cd95ddc0624b47cbe338749a0897e6487c83e9b
minkj1992 <minkj1992@gmail.com>
2021-04-22 21:28:58 +0900

    feat: pubSub module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PubSubModule } from './pub-sub/pub-sub.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      installSubscriptionHandlers: true,
+    }),
+    PubSubModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
